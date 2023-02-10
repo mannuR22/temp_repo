69492d0d7add44e90357d0b8861b2e14644cc144
unknown <mr2812910@gmail.com>
2023-02-10 16:07:02 +0530

    KYP Updated

---
+++ B2BUser.js
@@ -0,0 +1,16 @@
+
+db.LUsers.insertOne(
+{
+    
+    "userId": "n3a9ec07-e772-441e-ab71-02c7555dc479",
+    "phoneNumber": "+12222233333",
+    "name": "The Auntie Network",
+    "emailId": "theAuntieNetwork@email.com",
+    "gender": 1,
+    "timeZone": "US",
+    "ageGroup": 1,
+    "partnerCompanyName": "The Auntie Network",
+    "partnerLogoUrl": "https://hae-public-bucket.s3.ap-south-1.amazonaws.com/B2B_Images/auntieNetwork.png",
+    "role": 8,
+    "subdomainAlias": "insights"
+});
